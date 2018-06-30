90d5372d87474175bace14de593059be36936341
Lucas Rinaldi <rinaldi.lcs@gmail.com>
2018-06-30 19:01:35 -0300

    Reduce knexfile after new env flow

--- src/config/knexfile.js
+++ src/config/knexfile.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
 /* istanbul ignore next */
-if (process.env.NODE_ENV !== 'production') {
+if (!process.env.NODE_ENV) { // empty node env means is a command line execution
   const dotenv = require('dotenv-safe')
   dotenv.load({
     path: path.join(__dirname, '../../.env'),
@@ -10,64 +10,30 @@ if (process.env.NODE_ENV !== 'production') {
   })
 }
 
-module.exports = {
-  development: {
-    client: 'pg',
-    connection: {
-      host: process.env.PG_HOSTNAME,
-      user: process.env.PG_USERNAME,
-      password: process.env.PG_PASSWORD,
-      database: 'sac_api_server_dev',
-      port: process.env.PG_PORT,
-      charset: 'utf8'
-    },
-    migrations: {
-      directory: path.join(__dirname, '../services/db/migrations')
-    },
-    seeds: {
-      directory: path.join(__dirname, '../services/db/seeds')
-    }
+const commonConfig = {
+  client: 'pg',
+  connection: {
+    charset: 'utf8',
+    database: process.env.PG_DATABASE,
+    host: process.env.PG_HOST,
+    password: process.env.PG_PASSWORD,
+    port: process.env.PG_PORT,
+    user: process.env.PG_USER
   },
-  test: {
-    client: 'pg',
-    connection: {
-      host: process.env.PG_HOSTNAME,
-      user: process.env.PG_USERNAME,
-      password: process.env.PG_PASSWORD,
-      database: 'sac_api_server_test',
-      port: process.env.PG_PORT,
-      charset: 'utf8'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      directory: path.join(__dirname, '../services/db/migrations')
-    },
-    seeds: {
-      directory: path.join(__dirname, '../services/db/seeds')
-    }
+  migrations: {
+    directory: path.join(__dirname, '../services/db/migrations')
   },
-  production: {
-    client: 'pg',
-    connection: process.env.DATABASE_URL || {
-      host: process.env.PG_HOSTNAME,
-      user: process.env.PG_USERNAME,
-      password: process.env.PG_PASSWORD,
-      database: 'sac_api_server',
-      port: process.env.PG_PORT,
-      charset: 'utf8'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      directory: path.join(__dirname, '../services/db/migrations')
-    },
-    seeds: {
-      directory: path.join(__dirname, '../services/db/seeds')
-    }
+  pool: {
+    min: 2,
+    max: 10
+  },
+  seeds: {
+    directory: path.join(__dirname, '../services/db/seeds')
   }
+}
+
+module.exports = {
+  development: commonConfig,
+  test: commonConfig,
+  production: commonConfig
 }

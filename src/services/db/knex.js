165ee09fbd32fe3cf7604a0c95c9f549f14492c2
Lucas Rinaldi <rinaldi.lcs@gmail.com>
2018-06-30 20:57:10 -0300

    Implement a knex service which creates a singleton

--- src/services/db/knex.js
+++ src/services/db/knex.js
@@ -1,4 +1,35 @@
 import Knex from 'knex'
-import { knex } from '../../config'
+import { knex as knexConfig } from '../../config'
 
-export default Knex(knex)
+let knex = null
+
+export function init () {
+  if (knex) {
+    throw new Error("There's already a knex database connection.")
+  }
+  knex = Knex(knexConfig)
+  return knex
+}
+
+export async function destroy () {
+  if (!knex) {
+    throw new Error("There's no knex database connection.")
+  }
+  await knex.destroy()
+  const conn = knex
+  knex = null
+  return conn
+}
+
+export function get () {
+  if (!knex) {
+    throw new Error("There's no knex database connection.")
+  }
+  return knex
+}
+
+export default {
+  init,
+  destroy,
+  get
+}

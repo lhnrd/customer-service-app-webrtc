9439dee7f5497a584bd1479584253d849d297e91
Lucas Rinaldi <rinaldi.lcs@gmail.com>
2018-07-02 01:28:37 -0300

    Override Model’s createValidator method
    Now models that extend the BaseModel class will validate against Joi
    instead of the default validation.

--- src/common/base-model.js
+++ src/common/base-model.js
@@ -3,19 +3,18 @@ import guid from 'objection-guid'
 import visibility from 'objection-visibility'
 import { timestampPlugin as timestamps } from 'objection-timestamps'
 
+import JoiValidator from './joi-validator'
+
 const enhance = compose(guid(), timestamps(), visibility)
+const validator = new JoiValidator()
 
 class BaseModel extends enhance(Model) {
   static get columnNameMappers () {
     return snakeCaseMappers()
   }
 
-  $beforeInsert () {
-    this.createdAt = new Date().toISOString()
-  }
-
-  $beforeUpdate () {
-    this.updatedAt = new Date().toISOString()
+  static createValidator () {
+    return validator
   }
 }
 

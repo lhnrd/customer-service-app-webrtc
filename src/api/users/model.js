ca62321849e95e32deccc1692bacdabc5f6b55d3
Lucas Rinaldi <rinaldi.lcs@gmail.com>
2018-07-02 01:32:56 -0300

    Create UserSchema using Joi

--- src/api/users/model.js
+++ src/api/users/model.js
@@ -1,16 +1,27 @@
+import Joi from 'joi'
 import { compose } from 'objection'
 import password from 'objection-password'
 
 import BaseModel from '../../common/base-model'
 
 const enhance = compose(password())
+
+export const UserSchema = Joi.object({
+  id: Joi.string().uuid(),
+  email: Joi.string().email().required(),
+  name: Joi.string(),
+  role: Joi.string().valid(['admin', 'user']),
+  password: Joi.string().min(6).required()
+})
+
 class User extends enhance(BaseModel) {
   static get tableName () {
     return 'users'
   }
-  static get jsonSchema () { }
 
-  $beforeInsert () { }
+  static get schema () {
+    return UserSchema
+  }
 }
 
 export default User

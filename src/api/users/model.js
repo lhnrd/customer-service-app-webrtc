65d730cd90f2c7f63f2e08a8ba8e2bb31212be1b
Lucas Rinaldi <rinaldi.lcs@gmail.com>
2018-07-09 23:40:06 -0300

    Export user roles

--- src/api/users/model.js
+++ src/api/users/model.js
@@ -6,11 +6,13 @@ import BaseModel from '../../common/base-model'
 
 const enhance = compose(password())
 
+export const USER_ROLE_OPTIONS = ['admin', 'user']
+
 export const UserSchema = Joi.object({
   id: Joi.string().uuid(),
   email: Joi.string().email().required(),
   name: Joi.string(),
-  role: Joi.string().valid(['admin', 'user']),
+  role: Joi.string().valid(USER_ROLE_OPTIONS).default('user'),
   password: Joi.string().min(6).required()
 })
 

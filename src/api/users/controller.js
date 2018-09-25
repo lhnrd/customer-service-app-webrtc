e7297ae108da6c07d7289968dab4b17c163f7afd
Lucas Rinaldi <rinaldi.lcs@gmail.com>
2018-09-24 23:26:03 -0300

    Add all read functions

--- src/api/users/controller.js
+++ src/api/users/controller.js
@@ -1,15 +1,20 @@
 import User from './model'
-import { success } from '../../services/response'
+import { notFound, success } from '../../services/response'
+
+export const read = async ({ params }, res, next) =>
+  User
+    .query()
+    .findById(params.id)
+    .then(notFound(res))
+    .then(success(res))
+    .catch(next)
 
 export const readAll = (req, res, next) =>
   User
     .query()
     .select()
     .then(success(res))
+    .catch(next)
 
-export const read = ({ params }, res, next) =>
-  User
-    .query()
-    .where('id', params.id)
-    .first()
-    .then(success(res))
+export const readMe = ({ user }, res, next) =>
+  success(res)(user)

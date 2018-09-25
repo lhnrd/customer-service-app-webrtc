c5f0619490e6477c31c418d2055f89592f1068d3
Lucas Rinaldi <rinaldi.lcs@gmail.com>
2018-09-25 01:45:42 -0300

    Write all CREATE API methods

--- src/api/users/index.js
+++ src/api/users/index.js
@@ -1,6 +1,6 @@
 import { Router } from 'express'
-import { read, readAll, readMe } from './controller'
-import { token } from '../../services/passport'
+import { create, read, readAll, readMe } from './controller'
+import { master, token } from '../../services/passport'
 
 const router = new Router()
 
@@ -43,5 +43,25 @@ router.get('/me',
  */
 router.get('/:id',
   read)
+
+/**
+* @api {post} /users Create user
+* @apiName CreateUser
+* @apiGroup User
+* @apiPermission master
+* @apiParam {String} access_token Master access_token.
+* @apiParam {String} email User's email.
+* @apiParam {String{6..}} password User's password.
+* @apiParam {String} [name] User's name.
+* @apiParam {String} [picture] User's picture.
+* @apiParam {String=user,admin} [role=user] User's role.
+* @apiSuccess (Sucess 201) {Object} user User's data.
+* @apiError {Object} 400 Some parameters may contain invalid values.
+* @apiError 401 Master access only.
+* @apiError 409 Email already registered.
+*/
+router.post('/',
+  master(),
+  create)
 
 export default router
--- src/api/users/index.test.js
+++ src/api/users/index.test.js
@@ -1,5 +1,5 @@
 import request from 'supertest'
-import { apiRoot } from '../../config'
+import { apiRoot, masterKey } from '../../config'
 import express from '../../services/express'
 import { signSync } from '../../services/jwt'
 import routes from '.'
@@ -14,7 +14,7 @@ beforeAll(async () => {
 
   admin = await User.query().insert({
     name: 'Admin',
-    email: 'admin@email.com',
+    email: 'useradmin@email.com',
     password: '123456',
     role: 'admin'
   }).returning('*')
@@ -44,7 +44,6 @@ test('GET /users 200 (admin)', async () => {
   const { body, headers, status } = await request(app())
     .get(apiRoot)
     .query({ access_token: adminSession })
-
   expect(status).toBe(200)
   expect(headers['content-type']).toMatch(/json/)
   expect(Array.isArray(body)).toBe(true)
@@ -101,4 +100,78 @@ test('GET /users/:id 404', async () => {
   const { status } = await request(app())
     .get(apiRoot + '/5a21b571-c60f-4d1c-abfd-23e06900cb2e')
   expect(status).toBe(404)
+})
+
+test('POST /users 201 (master)', async () => {
+  const email = 'user@email.com'
+  const { status, body } = await request(app())
+    .post(apiRoot)
+    .send({ access_token: masterKey, email, password: '123456', role: 'user' })
+  expect(status).toBe(201)
+  expect(typeof body).toBe('object')
+  expect(body.email).toBe(email)
+})
+
+test('POST /users 201 (master)', async () => {
+  const email = 'admin@email.com'
+  const { status, body } = await request(app())
+    .post(apiRoot)
+    .send({ access_token: masterKey, email, password: '123456', role: 'admin' })
+  expect(status).toBe(201)
+  expect(typeof body).toBe('object')
+  expect(body.email).toBe(email)
+})
+
+test('POST /users 409 (master) - duplicated email', async () => {
+  const { status, body } = await request(app())
+    .post(apiRoot)
+    .send({ access_token: masterKey, email: 'userone@email.com', password: '123456' })
+  expect(status).toBe(409)
+  expect(typeof body).toBe('object')
+  expect(body.errors[0].param).toBe('email')
+})
+
+test('POST /users 400 (master) - invalid email', async () => {
+  const { status, body } = await request(app())
+    .post(apiRoot)
+    .send({ access_token: masterKey, email: 'invalid', password: '123456' })
+  expect(status).toBe(400)
+  expect(typeof body).toBe('object')
+  expect(body.errors[0].param).toBe('email')
+})
+
+test('POST /users 400 (master) - missing email', async () => {
+  const { status, body } = await request(app())
+    .post(apiRoot)
+    .send({ access_token: masterKey, password: '123456' })
+  expect(status).toBe(400)
+  expect(typeof body).toBe('object')
+  expect(body.errors[0].param).toBe('email')
+})
+
+test('POST /users 400 (master) - invalid password', async () => {
+  const { status, body } = await request(app())
+    .post(apiRoot)
+    .send({ access_token: masterKey, email: 'd@d.com', password: '123' })
+  expect(status).toBe(400)
+  expect(typeof body).toBe('object')
+  expect(body.errors[0].param).toBe('password')
+})
+
+test('POST /users 400 (master) - missing password', async () => {
+  const { status, body } = await request(app())
+    .post(apiRoot)
+    .send({ access_token: masterKey, email: 'd@d.com' })
+  expect(status).toBe(400)
+  expect(typeof body).toBe('object')
+  expect(body.errors[0].param).toBe('password')
+})
+
+test('POST /users 400 (master) - invalid role', async () => {
+  const { status, body } = await request(app())
+    .post(apiRoot)
+    .send({ access_token: masterKey, email: 'd@d.com', password: '123456', role: 'invalid' })
+  expect(status).toBe(400)
+  expect(typeof body).toBe('object')
+  expect(body.errors[0].param).toBe('role')
 })

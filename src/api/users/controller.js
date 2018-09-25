96fc15fbf20f20667b8c92c859c8ea90f737ac62
Lucas Rinaldi <rinaldi.lcs@gmail.com>
2018-09-25 01:45:49 -0300

    Implement create controller

--- src/api/users/controller.js
+++ src/api/users/controller.js
@@ -1,7 +1,44 @@
 import User from './model'
 import { notFound, success } from '../../services/response'
 
-export const read = async ({ params }, res, next) =>
+const createUser = body => ({
+  email: body.email,
+  password: body.password,
+  role: body.role
+})
+
+export const create = ({ body }, res, next) =>
+  User
+    .query()
+    .insert(createUser(body))
+    .then(success(res, 201))
+    .catch(error => {
+      if (error.name === 'error' && error.code === '23505') { // database validation error
+        res.status(409).json({
+          valid: false,
+          errors: [
+            {
+              param: 'email',
+              message: '"email" already registered'
+            }
+          ]
+        })
+      } else if (error.name === 'ValidationError' && error.isJoi) {
+        const errors = error.details.map(e => ({
+          param: e.context.key,
+          message: e.message
+        }))
+
+        res.status(400).json({
+          valid: false,
+          errors
+        })
+      } else {
+        next(error)
+      }
+    })
+
+export const read = ({ params }, res, next) =>
   User
     .query()
     .findById(params.id)

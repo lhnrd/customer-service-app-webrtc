05f30e1acd19619fa84e503d28f7cff2464212e8
Lucas Rinaldi <rinaldi.lcs@gmail.com>
2018-09-26 18:57:10 -0300

    Rename somethings

--- src/services/passport/index.js
+++ src/services/passport/index.js
@@ -18,7 +18,7 @@ export const password = () => (req, res, next) =>
     } else if (!user) {
       return res.status(401).end()
     }
-    req.logIn(user, { session: false }, (err) => {
+    req.login(user, { session: false }, (err) => {
       if (err) return res.status(401).end()
       next()
     })
@@ -32,7 +32,7 @@ export const token = ({ required, roles = USER_ROLE_OPTIONS } = {}) => (req, res
     if (err || (required && !user) || (required && !roles.includes(user.role))) {
       return res.status(401).end()
     }
-    req.logIn(user, { session: false }, (err) => {
+    req.login(user, { session: false }, (err) => {
       if (err) return res.status(401).end()
       next()
     })
@@ -76,11 +76,12 @@ passport.use('token', new JwtStrategy({
   jwtFromRequest: ExtractJwt.fromExtractors([
     ExtractJwt.fromUrlQueryParameter('access_token'),
     ExtractJwt.fromBodyField('access_token'),
-    ExtractJwt.fromAuthHeaderWithScheme('Bearer')
+    ExtractJwt.fromAuthHeaderAsBearerToken()
   ])
 }, ({ id }, done) =>
-  User.query().findById(id).then(user => {
-    done(null, user)
-    return null
-  }).catch(done)
+  User
+    .query()
+    .findById(id)
+    .then(user => done(null, user))
+    .catch(done)
 ))

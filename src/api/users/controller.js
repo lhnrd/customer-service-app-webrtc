4a800dae757ee96d964c2da7fb47e77043d9adfa
Lucas Rinaldi <rinaldi.lcs@gmail.com>
2018-09-27 00:07:26 -0300

    Better handle errors and also update pswd

--- src/api/users/controller.js
+++ src/api/users/controller.js
@@ -1,39 +1,16 @@
 import pick from 'lodash.pick'
 import User from './model'
-import { notFound, success } from '../../services/response'
+import { error, notFound, success } from '../../services/response'
 
-const USER_DATA = ['email', 'name', 'role']
+const CREATE_DATA = ['email', 'name', 'password', 'role']
+const UPDATE_DATA = ['email', 'name', 'role']
 
 export const create = ({ body }, res, next) =>
   User
     .query()
-    .insert(pick(body, USER_DATA))
+    .insert(pick(body, CREATE_DATA))
     .then(success(res, 201))
-    .catch(error => {
-      if (error.name === 'error' && error.code === '23505') { // database validation error
-        res.status(409).json({
-          valid: false,
-          errors: [
-            {
-              param: 'email',
-              message: '"email" already registered'
-            }
-          ]
-        })
-      } else if (error.name === 'ValidationError' && error.isJoi) {
-        const errors = error.details.map(e => ({
-          param: e.context.key,
-          message: e.message
-        }))
-
-        res.status(400).json({
-          valid: false,
-          errors
-        })
-      } else {
-        next(error)
-      }
-    })
+    .catch(error(res, next))
 
 export const read = ({ params }, res, next) =>
   User
@@ -41,37 +18,59 @@ export const read = ({ params }, res, next) =>
     .findById(params.id)
     .then(notFound(res))
     .then(success(res))
-    .catch(next)
+    .catch(error(res))
 
 export const readAll = (req, res, next) =>
   User
     .query()
     .select()
     .then(success(res))
-    .catch(next)
+    .catch(error(res))
 
 export const readMe = ({ user }, res, next) =>
   success(res)(user)
 
 export const update = ({ body, params, user }, res, next) => {
+  const userId = params.id === 'me' ? user.id : params.id
   const isAdmin = user.role === 'admin'
-  const isCurrentUser = user.id === params.id
+  const isCurrentUser = userId === user.id
 
   if (isAdmin || isCurrentUser) {
     return User
       .query()
-      .patch(pick(body, USER_DATA))
-      .where('id', params.id === 'me' ? user.id : params.id)
+      .patch(pick(body, UPDATE_DATA))
+      .where('id', userId)
       .returning('*')
       .first()
       .then(notFound(res))
       .then(success(res))
-      .catch(next)
+      .catch(error(res))
   }
   return res.status(401).json({
     message: 'You can\'t change other user\'s data',
-    param: 'password',
     valid: false
+  })
+}
+
+export const updatePassword = ({ body, params, user }, res, next) => {
+  const userId = params.id === 'me' ? user.id : params.id
+  const isCurrentUser = userId === user.id
+
+  if (isCurrentUser) {
+    return User
+      .query()
+      .patch(pick(body, ['password']))
+      .where('id', userId)
+      .returning('*')
+      .first()
+      .then(notFound(res))
+      .then(success(res))
+      .catch(error(res))
+  }
+  return res.status(401).json({
+    valid: false,
+    param: 'password',
+    message: 'You can\'t change other user\'s password'
   })
 }
 
@@ -84,4 +83,4 @@ export const destroy = ({ params }, res, next) =>
     .first()
     .then(notFound(res))
     .then(success(res))
-    .catch(next)
+    .catch(error(res))

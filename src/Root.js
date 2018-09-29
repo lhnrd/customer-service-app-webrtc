4d5bdf9d268353fe8c55f1854bf5e947c50797cb
Lucas Rinaldi <rinaldi.lcs@gmail.com>
2018-09-28 22:15:26 -0300

    Configure router and Root

---
+++ src/Root.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import App from './containers/App';
+
+const Root = () => (
+  <Router>
+    <App />
+  </Router>
+);
+
+export default Root;

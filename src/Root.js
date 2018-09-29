9c0ba062dde42973b0666fe9df0f5a2421417aa2
Lucas Rinaldi <rinaldi.lcs@gmail.com>
2018-09-29 19:43:34 -0300

    Setup styled-components

--- src/Root.js
+++ src/Root.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { ThemeProvider } from 'styled-components';
 
 import configureStore from './store/configureStore';
 import App from './containers/App';
 
 const store = configureStore();
+const theme = {
+  main: 'mediumseagreen',
+};
 
 const Root = () => (
   <Provider store={store}>
-    <Router>
-      <App />
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <App />
+      </Router>
+    </ThemeProvider>
   </Provider>
 );
 

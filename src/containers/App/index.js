d76ec5dda9d7f7d4354aff67ac169ad85d1cfba6
Lucas Rinaldi <rinaldi.lcs@gmail.com>
2018-09-29 19:43:43 -0300

    Setup tests

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -2,12 +2,20 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import { APP_PATH, LOGIN_PATH } from 'src/routes/paths';
+import Button from 'src/styles/Button';
 
 const App = () => (
   <div id="app">
     <Switch>
       <Route path={LOGIN_PATH} render={() => <div>Login</div>} />
-      <Route path={APP_PATH} render={() => <div>APP</div>} />
+      <Route
+        path={APP_PATH}
+        render={() => (
+          <div>
+            <Button>Test</Button>
+          </div>
+        )}
+      />
     </Switch>
   </div>
 );

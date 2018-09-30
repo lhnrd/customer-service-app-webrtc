a174aed9c7134aba154a5eb65432a8f7cd259ea8
Lucas Rinaldi <rinaldi.lcs@gmail.com>
2018-09-30 19:47:48 -0300

    Use login form inside page

--- src/pages/LoginPage/index.js
+++ src/pages/LoginPage/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Layout } from 'antd';
+import { Box } from '@rebass/grid';
 
 import FullScreenContainer from 'src/styles/FullScreenContainer';
-import { Box } from '@rebass/grid';
+import LoginForm from 'src/components/LoginForm';
 
 const LoginPage = () => (
   <Layout>
     <Layout.Content>
       <FullScreenContainer alignItems="center" justifyContent="center">
-        <Box width={1 / 5} />
+        <Box width={1 / 5}>
+          <LoginForm />
+        </Box>
       </FullScreenContainer>
     </Layout.Content>
   </Layout>

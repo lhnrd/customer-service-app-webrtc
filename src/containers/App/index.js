45e2f7ced781b8d1b8609f13cb9957a397b04e1e
Lucas Rinaldi <rinaldi.lcs@gmail.com>
2018-10-12 17:31:22 -0300

    Update readme and use fragment diamond syntax

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { Switch } from 'react-router-dom';
 
 import { HOME_PATH, LOGIN_PATH } from 'src/routes/paths';
@@ -9,13 +9,13 @@ import RouteAuthenticated from 'src/components/RouteAuthenticated';
 import RouteUnauthenticated from 'src/components/RouteUnauthenticated';
 
 const App = () => (
-  <Fragment>
+  <>
     <Global />
     <Switch>
       <RouteUnauthenticated path={LOGIN_PATH} component={LoginPage} />
       <RouteAuthenticated path={HOME_PATH} component={HomePage} />
     </Switch>
-  </Fragment>
+  </>
 );
 
 export default App;

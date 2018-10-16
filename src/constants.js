f61b54b626a4314ad02501eca6fb0fe7e9bcb4e6
Lucas Rinaldi <rinaldi.lcs@gmail.com>
2018-10-16 17:48:54 -0300

    Create constants

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,7 @@
+import createConstants from './utils/create-constants';
+
 export const AUTH_TOKEN_KEY = 'jwt_token'; // eslint-disable-line
+export const FSA = '@@socket-middleware/FSA';
 export const RSSA = '@@socket-middleware/RSSA';
+
+export const actions = createConstants()('START_CALL', 'CANCEL_CALL');

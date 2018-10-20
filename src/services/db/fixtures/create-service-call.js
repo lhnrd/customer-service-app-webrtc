c2c921c2e237f6d00d6b3b9efa7a5a232203e810
Lucas Rinaldi <rinaldi.lcs@gmail.com>
2018-10-20 20:14:29 -0300

    Improve service call fixture

--- src/services/db/fixtures/create-service-call.js
+++ src/services/db/fixtures/create-service-call.js
@@ -2,27 +2,26 @@ import faker from 'faker'
 import addMinutes from 'date-fns/fp/addMinutes'
 import ServiceCall from '../../../api/service-calls/model'
 
-const add30Minutes = addMinutes(30)
 const startedAtDate = new Date()
 
 export const createServiceCallDry = ({
   id = faker.random.uuid(),
   description = faker.lorem.sentences(3),
-  callRating = faker.random.number(6),
-  serviceRating = faker.random.number(6),
+  callRating = faker.random.number(5),
+  serviceRating = faker.random.number(5),
   isSolved = faker.random.boolean(),
   startedAt = startedAtDate,
-  endedAt = add30Minutes(startedAtDate),
+  endedAt = addMinutes(faker.random.number(120), startedAtDate),
   customerId,
   userId
 } = {}) => ({
   id,
   description,
-  callRating,
-  serviceRating,
-  isSolved,
-  startedAt,
-  endedAt,
+  callRating: (userId && endedAt) ? callRating : 0,
+  serviceRating: (userId && endedAt) ? serviceRating : 0,
+  isSolved: (userId && endedAt) ? isSolved : false,
+  startedAt: userId ? startedAt : undefined,
+  endedAt: userId ? endedAt : undefined,
   customerId,
   userId
 })

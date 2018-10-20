a6fbec743611995cab0287fbb369dbc3bb33ebe0
Lucas Rinaldi <rinaldi.lcs@gmail.com>
2018-10-20 00:54:38 -0300

    Add defaults to rating

--- src/services/db/migrations/20181018232751_create_service_calls.js
+++ src/services/db/migrations/20181018232751_create_service_calls.js
@@ -2,8 +2,8 @@ exports.up = function (knex, Promise) {
   return knex.schema.createTable('service_calls', function (table) {
     table.uuid('id').primary()
     table.string('description')
-    table.integer('call_rating')
-    table.integer('service_rating')
+    table.integer('call_rating').defaultTo(0)
+    table.integer('service_rating').defaultTo(0)
     table.boolean('is_solved').defaultTo(false)
     table.time('started_at')
     table.time('ended_at')

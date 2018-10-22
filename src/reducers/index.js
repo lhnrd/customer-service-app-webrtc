a4a7ea8eae9d910a30a7f97fa1cc61e738a8f968
Lucas Rinaldi <rinaldi.lcs@gmail.com>
2018-10-22 18:02:08 -0300

    Change auth and socket reducers to be more standard

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,15 @@
 import { combineReducers } from 'redux';
 
-import auth from './auth';
-import socket from './socket';
+import auth, { STATE_KEY as AUTH_STATE_KEY } from './auth';
+import serviceCalls, {
+  STATE_KEY as SERVICE_CALLS_STATE_KEY,
+} from './service-call';
+import socket, { STATE_KEY as SOCKET_STATE_KEY } from './socket';
 
 const rootReducer = combineReducers({
-  auth,
-  socket,
+  [AUTH_STATE_KEY]: auth,
+  [SERVICE_CALLS_STATE_KEY]: serviceCalls,
+  [SOCKET_STATE_KEY]: socket,
 });
 
 export default rootReducer;

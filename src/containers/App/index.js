c34845f13caa7cb68ef8d576188a5b8dfde284ef
Lucas Rinaldi <rinaldi.lcs@gmail.com>
2018-10-22 18:04:27 -0300

    Improve layout

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -3,7 +3,7 @@ import { Switch } from 'react-router-dom';
 
 import { HOME_PATH, LOGIN_PATH } from 'src/routes/paths';
 import Global from 'src/styles/Global';
-import HomePage from 'src/pages/HomePage';
+import HomeRoute from 'src/routes/HomeRoute';
 import LoginPage from 'src/pages/LoginPage';
 import RouteAuthenticated from 'src/components/RouteAuthenticated';
 import RouteUnauthenticated from 'src/components/RouteUnauthenticated';
@@ -13,7 +13,7 @@ const App = () => (
     <Global />
     <Switch>
       <RouteUnauthenticated path={LOGIN_PATH} component={LoginPage} />
-      <RouteAuthenticated path={HOME_PATH} component={HomePage} />
+      <RouteAuthenticated path={HOME_PATH} component={HomeRoute} />
     </Switch>
   </>
 );

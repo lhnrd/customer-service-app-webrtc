8d24c067eb52e3a1d9879dbccd3b927d9f08b9e7
Lucas Rinaldi <rinaldi.lcs@gmail.com>
2018-10-25 18:49:44 -0300

    Fully load socketthings

--- src/utils/socket-middleware.js
+++ src/utils/socket-middleware.js
@@ -1,39 +1,60 @@
-import io from 'socket.io-client';
-
+import { types } from 'src/actions/socket';
 import { FSA, RSSA } from 'src/constants';
 
-const eventsRoot = process.env.REACT_APP_EVENTS_ROOT || '';
+const {
+  SOCKET_OPEN_REQUEST,
+  SOCKET_OPEN_SUCCESS,
+  SOCKET_CLOSE_REQUEST,
+  SOCKET_CLOSE_SUCCESS,
+  SOCKET_CONN_ERROR,
+} = types;
 
-const createSocketMiddleware = () => {
-  const socket = io('/caller', {
-    path: eventsRoot,
-    transports: ['websocket'],
-  });
-  socket.on('connect', () => {
-    console.log('WebSocket connected.'); // eslint-disable-line
-  });
+const createSocketMiddleware = socket => ({ dispatch }) => {
+  socket.on(FSA, dispatch);
 
-  return ({ dispatch }) => {
-    socket.on(FSA, dispatch);
+  return next => action => {
+    const socketAction = action[RSSA];
 
-    return next => action => {
-      const socketAction = action[RSSA];
-
-      if (socketAction) {
-        const { ack, event, message, optimistic } = socketAction;
-        message.meta = {
-          ...message.meta,
-          sid: socket.id,
-        };
-        socket.emit(event || FSA, message, ack);
-        if (optimistic) {
-          next(message);
-        }
-        return;
+    if (socketAction) {
+      const { ack, event, message, optimistic } = socketAction;
+      message.meta = {
+        ...message.meta,
+        sid: socket.id,
+      };
+      socket.emit(event || FSA, message, ack);
+      if (optimistic) {
+        dispatch(message);
       }
+      return;
+    }
 
-      next(action);
-    };
+    switch (action.type) {
+      case SOCKET_OPEN_REQUEST:
+        socket.connect();
+
+        socket.on('connect', () =>
+          dispatch({
+            type: SOCKET_OPEN_SUCCESS,
+            payload: { sid: socket.id },
+          })
+        );
+        socket.on('disconnect', () => dispatch({ type: SOCKET_CLOSE_SUCCESS }));
+        socket.on('error', error =>
+          dispatch({ type: SOCKET_CONN_ERROR, payload: { error } })
+        );
+
+        break;
+      case SOCKET_CLOSE_REQUEST:
+        socket.disconnect();
+        break;
+      case SOCKET_CONN_ERROR:
+        socket.connect();
+        break;
+      default:
+        break;
+    }
+
+    next(action);
   };
 };
 

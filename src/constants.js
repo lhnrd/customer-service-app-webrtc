3658e81e4c38ef1b521dcd34110037f25f785adb
Lucas Rinaldi <rinaldi.lcs@gmail.com>
2018-10-25 18:34:09 -0300

    Create rtc constants

--- src/constants.js
+++ src/constants.js
@@ -1,8 +1,11 @@
 import createConstants from './utils/create-constants'
 
-export const FSA = '@@socket-middleware/FSA'
+export const FSA = '@@socket/FSA'
 
-export const events = createConstants()(
-  'START_CALL',
-  'CANCEL_CALL'
+export const rtcEvents = createConstants('@@rtc/')(
+  'PEER_CONNECT',
+  'PEER_SET',
+  'STREAM_SET',
+  'SIGNAL_RECEIVE',
+  'SIGNAL_SEND'
 )

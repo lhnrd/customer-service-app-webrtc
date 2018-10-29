f7b70e4c858de02c2b231c5d28f2344322c7b328
Lucas Rinaldi <rinaldi.lcs@gmail.com>
2018-10-29 20:27:08 -0300

    Use entity prop on payload

--- src/api/service-calls/events.js
+++ src/api/service-calls/events.js
@@ -4,14 +4,20 @@ import ServiceCall from './model'
 export default io => {
   ServiceCall.events.on('POST', ({ data }) => {
     io.of('/manager').emit(FSA, {
-      type: '@@service-call/CREATE_ENTITY_SUCCESS',
-      payload: data
+      type: '@@service-call/ENTITY_CREATE',
+      payload: { entity: data }
     })
   })
   ServiceCall.events.on('PUT', ({ data }) => {
     io.of('/manager').emit(FSA, {
-      type: '@@service-call/UPDATE_ENTITY_SUCCESS',
-      payload: data
+      type: '@@service-call/ENTITY_UPDATE',
+      payload: { entity: data }
+    })
+  })
+  ServiceCall.events.on('DELETE', ({ data }) => {
+    io.of('/manager').emit(FSA, {
+      type: '@@service-call/ENTITY_DELETE',
+      payload: { entity: data }
     })
   })
 }

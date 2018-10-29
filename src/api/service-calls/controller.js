75ba95717ace907d295d0555269f2cd6ca27c6a2
Lucas Rinaldi <rinaldi.lcs@gmail.com>
2018-10-29 20:25:52 -0300

    Add object-filter

--- src/api/service-calls/controller.js
+++ src/api/service-calls/controller.js
@@ -1,17 +1,11 @@
+import { buildFilter } from 'objection-filter'
 import ServiceCall from './model'
 import { error, success } from '../../services/response'
 
-export const readAll = ({ user }, res) =>
-  user.role === 'admin' ? (
-    ServiceCall
-      .query()
-      .select()
-      .then(success(res, 200))
-      .catch(error(res))
-  ) : (
-    ServiceCall
-      .query()
-      .where('userId', user.id)
-      .then(success(res, 200))
-      .catch(error(res))
-  )
+export const readAll = ({ user, query }, res) =>
+  buildFilter(ServiceCall)
+    .build(JSON.parse(query.filter))
+    .where('userId', user.id)
+    .orWhere('userId', null)
+    .then(success(res, 200))
+    .catch(error(res))

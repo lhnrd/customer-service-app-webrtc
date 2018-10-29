2d4c3127e8aa591cb2738b13fe313db6fe88dba1
Lucas Rinaldi <rinaldi.lcs@gmail.com>
2018-10-29 20:24:49 -0300

    Add service call actions

---
+++ src/actions/service-call.js
@@ -0,0 +1,49 @@
+import { RSSA } from 'src/constants';
+import createConstants from 'src/utils/create-constants';
+
+export const types = createConstants('@@service-call/')(
+  'ENTITY_CREATE',
+  'ENTITY_DELETE',
+  'ENTITY_UPDATE',
+  'SERVICE_CALL_SET_ACTIVE'
+);
+
+const {
+  ENTITY_CREATE,
+  ENTITY_DELETE,
+  ENTITY_UPDATE,
+  SERVICE_CALL_SET_ACTIVE,
+} = types;
+
+export const createServiceCall = ({ customerId }) => dispatch =>
+  dispatch({
+    [RSSA]: {
+      event: ENTITY_CREATE,
+      message: { data: { customerId } },
+      ack: (_, entity) => {
+        dispatch({ type: ENTITY_CREATE, payload: { entity } });
+        dispatch({
+          type: SERVICE_CALL_SET_ACTIVE,
+          payload: { serviceCallId: entity.id },
+        });
+      },
+    },
+  });
+
+export const updateServiceCall = data => dispatch =>
+  dispatch({
+    [RSSA]: {
+      event: ENTITY_UPDATE,
+      message: { data },
+      ack: (_, entity) => {
+        dispatch({ type: ENTITY_UPDATE, payload: { entity } });
+      },
+    },
+  });
+
+export const deleteServiceCall = ({ serviceCallId }) => ({
+  [RSSA]: {
+    event: ENTITY_DELETE,
+    message: { data: { serviceCallId } },
+  },
+});

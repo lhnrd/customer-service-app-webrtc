ba7585322dca4b31f376c50560b480c1cd820bb7
Lucas Rinaldi <rinaldi.lcs@gmail.com>
2018-11-26 12:05:05 -0200

    Organize manager events

--- src/events/manager.js
+++ src/events/manager.js
@@ -1,17 +1,40 @@
 import d from 'debug'
-import { FSA, rtcEvents } from '../constants'
+import ServiceCall from '../api/service-calls/model'
+import { FSA, rtcEvents, serviceCallEvents, socketEvents } from '../constants'
 
 const debug = d('socket:manager')
-const {
-  PEER_CONNECT,
-  SIGNAL_SEND
-} = rtcEvents
+
+export const ackSuccess = ack => entity => ack && ack(null, entity)
+export const ackError = ack => error => ack && ack(error)
 
 export default io => socket => {
   debug('socket connected')
 
-  socket.on(PEER_CONNECT, (message, ack) => {
-    debug(`${PEER_CONNECT} received`)
+  // Socket domain events
+  socket.on(socketEvents.DISCONNECT, async reason => {
+    debug(`socket disconnected ${reason}`)
+  })
+
+  socket.on(socketEvents.ERROR, error => {
+    debug(`socket error ${error}`)
+  })
+
+  // Service call events
+  socket.on(serviceCallEvents.ENTITY_UPDATE, ({ data }, ack) =>
+    ServiceCall
+      .query()
+      .patch(data)
+      .where('id', data.id)
+      .returning('*')
+      .then(entity => {
+        ackSuccess(ack)(entity)
+      })
+      .catch(ackError(ack))
+  )
+
+  // RTC events
+  socket.on(rtcEvents.PEER_CONNECT, (message, ack) => {
+    debug(`${rtcEvents.PEER_CONNECT} received`)
     const { meta: { room } } = message
 
     socket.join(room, () => {
@@ -19,8 +42,8 @@ export default io => socket => {
     })
   })
 
-  socket.on(SIGNAL_SEND, message => {
-    debug(`${SIGNAL_SEND} received`)
+  socket.on(rtcEvents.SIGNAL_SEND, message => {
+    debug(`${rtcEvents.SIGNAL_SEND} received`)
     const {
       meta: { namespace, room }
     } = message
@@ -28,13 +51,5 @@ export default io => socket => {
     debug(`emitting ${FSA} to ${room} and ${namespace}`)
 
     io.of(namespace).to(room).emit(FSA, message)
-  })
-
-  socket.on('disconnect', reason => {
-    debug(`socket disconnected ${reason}`)
-  })
-
-  socket.on('error', error => {
-    debug(`socket error ${error}`)
   })
 }
